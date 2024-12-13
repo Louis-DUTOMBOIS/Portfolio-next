b8c7b86094a0518923225262a6ede41afb552eb3
Louis Dutombois <louis.dutombois@gmail.com>
2024-12-13 14:54:50 +0100

    Ajout du portfolio et des nouvelles pages

--- app/NavBar/MobileNav.tsx
+++ app/NavBar/MobileNav.tsx
@@ -1,40 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router-dom";
-import AboutPage from "@/app/About/page";
-import ContactPage from "@/app/Contact/page";
-import ProjectsPage from "@/app/Projects/page";
-import HomePage from "@/app/page";
+import Link from "next/link";
 
 const MobileNav = () => {
     return (
-        <Router>
-            <nav className="bg-gray-800 p-4 text-white">
-                <ul className="flex justify-between">
-                    <li>
-                        <NavLink to="/" aria-current=
-                            "page" className="nav-link">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about" aria-current=
-                            "page" className="nav-link">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact" aria-current=
-                            "page" className="nav-link">Contact</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/projects" aria-current=
-                            "page" className="nav-link">Projects</NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="/about" element={<AboutPage/>}> About </Route>
-                <Route path="/contact" element={<ContactPage/>}>About</Route>
-                <Route path="/projects" element={<ProjectsPage/>}>About</Route>
-            </Routes>
-        </Router>
+        <nav className="bg-gray-800 p-4 text-white">
+            <ul className="flex justify-between">
+                <li>
+                    <Link href="/" aria-current=
+                        "page" className="nav-link">Home</Link>
+                </li>
+                <li>
+                    <Link href="/About/" aria-current=
+                        "page" className="nav-link">About</Link>
+                </li>
+                <li>
+                    <Link href="/Projects/" aria-current=
+                        "page" className="nav-link">Projects</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
