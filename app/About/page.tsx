4c69e8606edb609196e1b5e2a1afa8af2cf8b580
Louis Dutombois <louis.dutombois@gmail.com>
2024-12-26 23:30:01 +0100

    Mise a jour du portfolio

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
             <div className="relative mt-2 flex flex-col items-center justify-center h-full text-white text-center">
                 <Image src="/pp.png" alt="Profile Photo" width={300} height={150} className="rounded-full mb-4" />
                 <h1 className="text-5xl font-bold mb-4 text-violet">About Me</h1>
-                <p className="mt-4 text-gray-300">Louis DUTOMBOIS, 19 ans, étudiant à l'IUT de Lens, en BUT Informatique et apprenti développeur Web.</p>
+                <p className="mt-4 text-gray-300">Louis DUTOMBOIS, 19 ans, étudiant à l&apos;IUT de Lens, en BUT Informatique et apprenti développeur Web.</p>
             </div>
         </section>
     );
